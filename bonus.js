fdd4700f7fdd9cf0eb810cd30a4a3b1c1719d478
samuele301197 <parisisamu32@gmail.com>
2025-05-21 17:34:50 +0200

    feat:bonus-1

---
+++ bonus.js
@@ -0,0 +1,16 @@
+function contaVocali(stringa) {
+const vocali = ['a', 'e', 'i', 'o', 'u'];
+let count = 0;
+
+for (let i = 0; i < vocali.length; i++) {
+ if(vocali.includes(stringa[i])){
+count ++;
+ }   
+}
+return count;
+}
+
+const name = process.argv[2]
+console.log(contaVocali(name));
+
+
